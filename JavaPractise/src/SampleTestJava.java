59d4303b9e1e14d8f64274fe650258b340759469
kalyan <kalyan@nineleaps.com>
2017-03-19 13:17:16 +0530

    1st commit

---
+++ JavaPractise/src/SampleTestJava.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nineleaps on 13/3/17.
+ */
+public class SampleTestJava {
+
+    private static ArrayList<String> values;
+    private static String stringToConvert = "kms82jjs209mdm92jd39d";
+    private static int j = 3;
+    private static String firstValue;
+
+    public static void main(String args[]) {
+
+        splitFunction(stringToConvert);
+        printArrayListValues(values);
+        convertBinary(values);
+
+    }
+
+    private static void convertBinary(ArrayList<String> values) {
+        String temp;
+        byte[] bytes;
+        int val;
+        StringBuilder binary = new StringBuilder();
+
+
+        for (int j = 0; j < values.size(); j++) {
+
+
+            temp = values.get(j);
+            bytes = values.get(j).getBytes();
+
+            for (byte b : bytes) {
+                val = b;
+                for (int i = 0; i < 8; i++) {
+                    binary.append((val & 128) == 0 ? 0 : 1);
+                    val <<= 1;
+                }
+                binary.append("");
+
+            }
+            System.out.println(temp + ":" + binary);
+        }
+    }
+
+
+    private static void printArrayListValues(ArrayList<String> values) {
+        for (int i = 0; i < values.size(); i++) {
+            firstValue = values.get(i);
+            System.out.println(firstValue);
+        }
+
+    }
+
+    private static void splitFunction(String stringToConvert) {
+        values = new ArrayList<>();
+        for (int i = 0; i < stringToConvert.length(); i = i + 3) {
+            values.add(stringToConvert.substring(i, j));
+            j = j + 3;
+        }
+
+        //printing  array list values
+
+
+    }
+}
